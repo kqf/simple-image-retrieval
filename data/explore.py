ce240b28f632e2fb6327fc1ace33346f360c8029
Sasha <q.dev.null@gmail.com>
2021-08-13 21:20:30 +0200

    Add more sophisticated example

--- data/explore.py
+++ data/explore.py
@@ -4,9 +4,24 @@ from telethon import TelegramClient
 def main():
     api_id = 1
     api_hash = 'lol'
+    username = 'lol'
+    picture = 'pepe.jpg'
 
     client = TelegramClient('session_name', api_id, api_hash)
     client.start()
+
+    print(client.get_me().stringify())
+
+    client.send_message(username, 'Hello world!')
+    client.send_file(username, picture)
+
+    client.download_profile_photo('me')
+    messages = client.get_messages(username)
+    messages[0].download_media()
+
+    # @client.on(events.NewMessage(pattern='(?i)hi|hello'))
+    # async def handler(event):
+    #     await event.respond('Hey!')
 
 
 if __name__ == '__main__':
