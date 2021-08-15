63c4649dc4b9a140a161f290160064ede7fd7856
Sasha <q.dev.null@gmail.com>
2021-08-15 16:28:35 +0200

    Add the dummy reply

---
+++ replier.py
@@ -0,0 +1,23 @@
+import logging
+
+from telethon import TelegramClient, events
+from environs import Env
+
+logging.basicConfig(
+    format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+    level=logging.INFO)
+
+
+env = Env()
+env.read_env()
+
+
+client = TelegramClient('test', env("API_ID"), env("API_HASH"))
+
+
+@client.on(events.NewMessage)
+async def my_replier(event):
+    await event.reply('🔥')
+
+client.start()
+client.run_until_disconnected()
