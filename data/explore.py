355986706f39fda63ec52b7cd302b2102fcbb697
kqf <q.dev.null@gmail.com>
2021-08-15 16:27:29 +0200

    Explore channels (#9)
    * Bump the requirements
    
    * Create a list of atributes required
    
    * Convert the chat history to pandas

--- data/explore.py
+++ data/explore.py
@@ -1,11 +1,40 @@
-import asyncio
+import logging
+import pandas as pd
+from operator import attrgetter
 
-from telethon import TelegramClient
+from telethon.sync import TelegramClient
 from environs import Env
+logging.basicConfig(
+    format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+    level=logging.INFO)
 
 
 env = Env()
 env.read_env()
+
+fields = [
+    'entity.date',
+    'folder_id',
+    'id',
+    'is_channel',
+    'is_group',
+    'is_user',
+    'name',
+    'title',
+    'unread_count',
+    'unread_mentions_count'
+]
+
+
+def to_dict(dialog, fields):
+    dump = {}
+    for field in fields:
+        try:
+            dump[field] = attrgetter(field)(dialog)
+        except AttributeError:
+            dump[field] = None
+
+    return dump
 
 
 def main():
@@ -13,8 +42,10 @@ def main():
     # picture = 'pepe.jpg'
 
     with TelegramClient('test', env("API_ID"), env("API_HASH")) as client:
-        dialogs = asyncio.run(client.get_dialogs())
+        dialogs = [to_dict(d, fields) for d in client.get_dialogs()]
         print(dialogs)
+        df = pd.DataFrame(dialogs)
+        print(df)
 
         # print(client.get_me().stringify())
 
