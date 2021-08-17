0301b281dc2acf5ae55b7bbce5816ac91087688c
Sasha <q.dev.null@gmail.com>
2021-08-17 06:10:58 +0200

    MClean the exploration folder

--- data/explore.py
+++ data/explore.py
@@ -42,19 +42,6 @@ def main(titles, output):
         print(df)
         df.to_csv(output, sep="\t", index=False)
 
-        # print(client.get_me().stringify())
-
-        # client.send_message(username, 'Hello world!')
-        # client.send_file(username, picture)
-
-        # client.download_profile_photo('me')
-        # messages = client.get_messages(username)
-        # messages[0].download_media()
-
-        # @client.on(events.NewMessage(pattern='(?i)hi|hello'))
-        # async def handler(event):
-        #     await event.respond('Hey!')
-
 
 if __name__ == '__main__':
     main()
