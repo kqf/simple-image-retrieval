635170cb8f0c1ae18712a51b835f8a34191666ca
kqf <q.dev.null@gmail.com>
2021-08-18 06:38:13 +0200

    Add metadata to messages (#17)
    * Explore the message structure
    
    * Add the download logic
    
    * Add the metadata fields
    
    * Save the metadata to the output file
    
    * Fix the identation

--- data/fetch.py
+++ data/fetch.py
@@ -17,6 +17,8 @@ def main(target, output, photos):
     df = pd.read_csv(target, sep="\t")
     df["entity.date"] = pd.to_datetime(df["entity.date"])
     candidates = df[["title", "entity.date"]]
+
+    metadata = []
     with TelegramClient('test', env("API_ID"), env("API_HASH")) as client:
         for idx, (title, date) in candidates.iterrows():
             lpath = Path(photos) / title
@@ -32,6 +34,14 @@ def main(target, output, photos):
                 download_path = lpath / str(message.photo.access_hash)
                 fname = message.download_media(download_path)
                 print(fname)
+                metadata.append({
+                    "file": fname,
+                    "source": title,
+                })
+
+    odf = pd.DataFrame(metadata)
+    print(odf)
+    odf.to_csv(output)
 
 
 if __name__ == '__main__':
