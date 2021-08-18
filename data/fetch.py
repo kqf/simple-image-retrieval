cebac93db7e1edd3bd55ec7954cc6eb86b8785a0
kqf <q.dev.null@gmail.com>
2021-08-18 06:27:07 +0200

    Add the download logic (#16)
    * Explore the message structure
    
    * Add the download logic

--- data/fetch.py
+++ data/fetch.py
@@ -1,6 +1,7 @@
 import click
 import pandas as pd
 
+from pathlib import Path
 from environs import Env
 from telethon.sync import TelegramClient
 
@@ -11,20 +12,26 @@ env.read_env()
 @click.command()
 @click.option("--target", type=click.Path(exists=True), default="titles.txt")
 @click.option("--output", type=click.Path(exists=False), default="output.txt")
-def main(target, output):
+@click.option("--photos", type=click.Path(), default="photos")
+def main(target, output, photos):
     df = pd.read_csv(target, sep="\t")
     df["entity.date"] = pd.to_datetime(df["entity.date"])
     candidates = df[["title", "entity.date"]]
     with TelegramClient('test', env("API_ID"), env("API_HASH")) as client:
         for idx, (title, date) in candidates.iterrows():
+            lpath = Path(photos) / title
             entity = client.get_entity(title)
             messages = client.iter_messages(
                 entity=entity,
                 offset_date=date,
                 reverse=True,
             )
-            for message in messages:
-                pass
+            for i, message in enumerate(messages):
+                if i > 5:
+                    break
+                download_path = lpath / str(message.photo.access_hash)
+                fname = message.download_media(download_path)
+                print(fname)
 
 
 if __name__ == '__main__':
