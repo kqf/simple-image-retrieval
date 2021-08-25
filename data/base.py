750e8d7616ab32182bd6c09a56f90ba269154060
Sasha <q.dev.null@gmail.com>
2021-08-25 11:27:27 +0200

    Factor out the common parts

---
+++ data/base.py
@@ -0,0 +1,26 @@
+import logging
+import pandas as pd
+
+from environs import Env
+from telethon.sync import TelegramClient
+from contextlib import contextmanager
+from functools import partial
+
+logger = logging.getLogger(__name__)
+
+env = Env()
+env.read_env()
+telegram = partial(
+    TelegramClient,
+    api_id=env("API_ID"),
+    api_hash=env("API_HASH")
+)
+
+
+@contextmanager
+def dump_list(ofile):
+    data = []
+    yield data
+    df = pd.DataFrame(data)
+    logger.warning("Dumping the dataframe %s", df)
+    df.to_csv(ofile, index=False)
