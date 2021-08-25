03bcd997c70d3d9b211cca0cc1365e1d6b79c040
kqf <q.dev.null@gmail.com>
2021-08-25 11:36:19 +0200

    Refactor the downloads (#34)
    * Rename the anchor script
    
    * Bump the makefile
    
    * Update the source names
    
    * Factor out the common parts
    
    * Fix the code for positive image slection

--- data/anchors.py
+++ data/anchors.py
@@ -5,29 +5,10 @@ import pandas as pd
 
 from click import Path as cpth
 from pathlib import Path
-from environs import Env
-from telethon.sync import TelegramClient
-from contextlib import contextmanager
-from functools import partial
+from data.base import telegram, dump_list
 
 logger = logging.getLogger(__name__)
 
-env = Env()
-env.read_env()
-telegram = partial(
-    TelegramClient,
-    api_id=env("API_ID"),
-    api_hash=env("API_HASH")
-)
-
-
-@contextmanager
-def dump_list(ofile):
-    data = []
-    yield data
-    df = pd.DataFrame(data)
-    logger.warning("Dumping the dataframe %s", df)
-    df.to_csv(ofile, index=False)
 
 
 def candidates(df):
