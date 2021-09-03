d03f54ec0751f8dcbc0223e795678f0d29b3c626
Sasha <q.dev.null@gmail.com>
2021-09-03 20:38:22 +0200

    Print the examples

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -5,4 +5,5 @@ from model.dataset import SimilarityDataset
 def test_reads(fake_dataset):
     df = pd.read_table(fake_dataset)
     dataset = SimilarityDataset(df.loc)
-    print(dataset)
+    for example in dataset:
+        print(example)
