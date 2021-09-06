42e957dcb3c983bd83876a553c15cfb0d92bf1d0
kqf <q.dev.null@gmail.com>
2021-09-06 06:24:18 +0200

    Add the training loop (#63)
    * test against the batch of ones
    
    * Add the assertion on the output shapes
    
    * Add the missing requirements
    
    * Add the build model script
    
    * Fix a typo

--- model/model.py
+++ model/model.py
@@ -1,3 +1,4 @@
+import skorch
 import torch
 import torchvision
 
@@ -6,3 +7,19 @@ def classifier():
     backbone = torchvision.models.resnet50()
     backbone.fc = torch.nn.Linear(backbone.fc.in_features, 2)
     return backbone
+
+
+def build_model(lr=1e-4):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = skorch.NeuralNetClassifier(
+        module=classifier(),
+        criterion=torch.nn.CrossEntropyLoss,
+        optimizer=torch.optim.Adam,
+        optimizer__lr=lr,
+        max_epochs=2,
+        batch_size=256,
+        iterator_train__shuffle=True,
+        iterator_valid__shuffle=False,
+        device=device,
+    )
+    return model
