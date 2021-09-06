177d265adbed552e0b4836f3148c6aaa6b9711ac
Sasha <q.dev.null@gmail.com>
2021-09-06 06:22:58 +0200

    Fix a typo

--- model/model.py
+++ model/model.py
@@ -3,7 +3,7 @@ import torch
 import torchvision
 
 
-def classfifier():
+def classifier():
     backbone = torchvision.models.resnet50()
     backbone.fc = torch.nn.Linear(backbone.fc.in_features, 2)
     return backbone
@@ -12,7 +12,7 @@ def classfifier():
 def build_model(lr=1e-4):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = skorch.NeuralNetClassifier(
-        module=classfifier(),
+        module=classifier(),
         criterion=torch.nn.CrossEntropyLoss,
         optimizer=torch.optim.Adam,
         optimizer__lr=lr,
