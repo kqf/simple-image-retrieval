9399532ed815c577e9730531f39fc70f963fb185
kqf <q.dev.null@gmail.com>
2021-09-07 06:46:13 +0200

    Add a separate classifier module (#66)
    * Add a separate classifier module
    
    * Remove the functional definition

--- model/model.py
+++ model/model.py
@@ -3,16 +3,21 @@ import torch
 import torchvision
 
 
-def classifier():
-    backbone = torchvision.models.resnet50()
-    backbone.fc = torch.nn.Linear(backbone.fc.in_features, 2)
-    return backbone
+class Classifier(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        backbone = torchvision.models.resnet50()
+        backbone.fc = torch.nn.Linear(backbone.fc.in_features, 2)
+        self.backbone = backbone
+
+    def forward(self, x):
+        return self.backbone(x)
 
 
 def build_model(lr=1e-4):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = skorch.NeuralNetClassifier(
-        module=classifier(),
+        module=Classifier,
         criterion=torch.nn.CrossEntropyLoss,
         optimizer=torch.optim.Adam,
         optimizer__lr=lr,
