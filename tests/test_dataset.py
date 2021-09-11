6f37414e8e16bf318a93afc36927af9cd1b74aa2
Sasha <q.dev.null@gmail.com>
2021-09-11 10:15:34 +0200

    Add more meaningful checks for the similarity dataset

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 from model.dataset import SimilarityDataset
 
 
@@ -6,4 +8,7 @@ def test_reads(fake_dataset):
     df = pd.read_table(fake_dataset)
     dataset = SimilarityDataset(df.iloc)
     for i, (image, label) in enumerate(dataset):
-        print(image.shape, label)
+        assert label in {0, 1}
+        assert image.shape[0] == 3, "Dataset should contain 3 channel images"
+        plt.imshow(image.transpose(1, 2, 0), interpolation='nearest')
+        plt.show()
