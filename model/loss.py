330b16acd3abfbebd0aa84045070a5b9e0771237
Sasha <q.dev.null@gmail.com>
2021-09-12 09:13:48 +0200

    Fix the L2 distance

--- model/loss.py
+++ model/loss.py
@@ -9,22 +9,27 @@ def dist(a, b):
     return -2 * a @ b.T + l2(a) + l2(b)
 
 
+def dist2(a, b):
+    return torch.sum((a - b) ** 2, dim=-1)
+
+
 class RetrievalLoss(torch.nn.Module):
-    def __init__(self, sim=dist, delta=1.0):
+    def __init__(self, sim=dist2, delta=1.0):
         super().__init__()
         self.delta = delta
         self.sim = sim
 
     def forward(self, queries, targets):
         with torch.no_grad():
-            distances = self.sim(queries, queries)
+            distances = self.sim(queries[None, :], queries[:, None])
 
             # exploit the broadcasting
             same_idx = targets.view(-1, 1) == targets.view(1, -1)
-            pos = queries[(distances * same_idx).argmax(-1)]
+            pos_idx = (distances * same_idx).argmax(-1)
+            pos = queries[pos_idx]
 
             neg_idx = (distances * ~same_idx).argmax(-1)
             neg = queries[neg_idx]
 
         loss = self.delta - l2(queries - pos) + l2(queries - neg)
-        return torch.nn.functional.max(loss).mean()
+        return torch.nn.functional.relu(loss).mean()
