eceeea2e0b80d20c148f67e800b8cb111c6f4009
Sasha <q.dev.null@gmail.com>
2021-09-14 19:43:24 +0200

    Add the custom initializations

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -13,8 +13,19 @@ def batch(batch_size, channels=3, width=480, height=640):
     return torch.ones((batch_size, channels, width, height))
 
 
+def init_with(f):
+    def wrap(m):
+        if not isinstance(m, torch.nn.Conv2d):
+            f(m.weight.data)
+            f(m.bias.data)
+            return
+        f(m)
+    return wrap
+
+
 def test_classifier(batch, batch_size):
     clf = Classifier()
+    clf.apply(init_with(torch.nn.init.zeros_))
     predictions = clf(batch)
 
     # For now it's binary classification problem
