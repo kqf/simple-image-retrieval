c0eef7304f623894805ec3cc27fa9210e730dec9
kqf <q.dev.null@gmail.com>
2021-09-14 20:13:57 +0200

    Refactor image operations (#85)
    * Move write function to aseparate module
    
    * Simplify the dataset definition

--- model/dataset.py
+++ model/dataset.py
@@ -1,7 +1,7 @@
-import cv2
 import pathlib
 
 from torch.utils.data import Dataset
+from model.cv import read
 
 
 class SimilarityDataset(Dataset):
@@ -16,10 +16,7 @@ class SimilarityDataset(Dataset):
     def __getitem__(self, idx):
         sample = pathlib.Path(self.samples[idx]["image"])
 
-        # By default OpenCV uses BGR color space for color images,
-        # so we need to convert the image to RGB color space.
-        image_channels_last = cv2.imread(str(sample), cv2.COLOR_BGR2RGB)
-        image = image_channels_last.transpose(2, 0, 1)
+        image = read(sample)
         label = self.samples[idx]["label"]
 
         if self.transform is not None:
