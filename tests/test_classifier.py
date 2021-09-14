9fa4a3961fda5427894c7ae885cbbbb1429f0113
kqf <q.dev.null@gmail.com>
2021-09-14 20:01:46 +0200

    Add deterministic tests (#84)
    * Switch to hard negatives mining scheme
    
    * Reduce the margin
    
    * Add the custom initializations
    
    * Fix the initializations
    
    * Fix the tests

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -1,5 +1,6 @@
 import torch
 import pytest
+import numpy as np
 from model.model import Classifier
 
 
@@ -13,9 +14,28 @@ def batch(batch_size, channels=3, width=480, height=640):
     return torch.ones((batch_size, channels, width, height))
 
 
+def init_with(f):
+    def wrap(m):
+        if 'weight' in dir(m) and m.weight is not None:
+            f(m.weight)
+
+        if 'bias' in dir(m) and m.bias is not None:
+            f(m.bias)
+
+    return wrap
+
+
 def test_classifier(batch, batch_size):
     clf = Classifier()
-    predictions = clf(batch)
+    clf.apply(init_with(torch.nn.init.zeros_))
+    preds = clf(batch)
+
+    # Check if it's indeed convergent
+    np.testing.assert_allclose(preds.detach(), 0.)
 
     # For now it's binary classification problem
-    assert predictions.shape == (batch_size, 2)
+    assert preds.shape == (batch_size, 2)
+
+    clf.apply(init_with(torch.nn.init.ones_))
+    preds = clf(batch)
+    assert preds.shape == (batch_size, 2)
