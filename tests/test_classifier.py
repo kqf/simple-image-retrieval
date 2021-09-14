e897a954b571ad48382bb7252429f6dce2d37ad9
Sasha <q.dev.null@gmail.com>
2021-09-14 20:01:21 +0200

    Fix the tests

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -1,5 +1,6 @@
 import torch
 import pytest
+import numpy as np
 from model.model import Classifier
 
 
@@ -15,16 +16,26 @@ def batch(batch_size, channels=3, width=480, height=640):
 
 def init_with(f):
     def wrap(m):
-        if not isinstance(m, torch.nn.parameter.Parameter):
-            return
-        f(m)
+        if 'weight' in dir(m) and m.weight is not None:
+            f(m.weight)
+
+        if 'bias' in dir(m) and m.bias is not None:
+            f(m.bias)
+
     return wrap
 
 
 def test_classifier(batch, batch_size):
     clf = Classifier()
     clf.apply(init_with(torch.nn.init.zeros_))
-    predictions = clf(batch)
+    preds = clf(batch)
+
+    # Check if it's indeed convergent
+    np.testing.assert_allclose(preds.detach(), 0.)
 
     # For now it's binary classification problem
-    assert predictions.shape == (batch_size, 2)
+    assert preds.shape == (batch_size, 2)
+
+    clf.apply(init_with(torch.nn.init.ones_))
+    preds = clf(batch)
+    assert preds.shape == (batch_size, 2)
