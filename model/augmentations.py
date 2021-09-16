9cce879bd00265c44f72ebb30cff81ef373ef799
kqf <q.dev.null@gmail.com>
2021-09-16 07:48:32 +0200

    Fix the augmentations (#89)
    * Disable the random rotations
    
    * Release more augmentations
    
    * Remove the broken transofrmations

--- model/augmentations.py
+++ model/augmentations.py
@@ -21,18 +21,17 @@ def transform(train=True, mean=None, std=None):
     return alb.Compose([
         alb.HorizontalFlip(),
         alb.VerticalFlip(),
-        alb.RandomRotate90(),
+        # alb.RandomRotate90(), # TODO: Check me
         alb.ShiftScaleRotate(
             shift_limit=0.0625,
             scale_limit=0.2,
             rotate_limit=15,
             p=0.9,
             border_mode=cv2.BORDER_REFLECT),
-        # alb.OneOf([
-        #     alb.OpticalDistortion(p=0.3),
-        #     alb.GridDistortion(p=.1),
-            # alb.IAAPiecewiseAffine(p=0.3),
-        # ], p=0.3),
+        alb.OneOf([
+            alb.OpticalDistortion(p=0.3),
+            alb.GridDistortion(p=.1),
+        ], p=0.3),
         alb.OneOf([
             alb.HueSaturationValue(10, 15, 10),
             alb.CLAHE(clip_limit=2),
