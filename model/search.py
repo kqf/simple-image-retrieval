b99d5fe3cd27382957177fd60072b2e1cedc12d0
Sasha <q.dev.null@gmail.com>
2021-09-19 08:37:04 +0200

    Add the faiss index

--- model/search.py
+++ model/search.py
@@ -1,3 +1,4 @@
+import faiss                   # make faiss available
 import numpy as np
 
 
@@ -18,3 +19,12 @@ def linear(query, base, model, distance=l2, k=5):
 
     # Take top k vectors as answers
     return (np.argsort(distances, -1))[..., :k]
+
+
+def approximate(query, base, model, k=5):
+    base_ = model.predict(base)
+    query_ = model.predict(query)
+    index = faiss.IndexFlatL2(base_.shape[-1])
+    index.add(base_)
+    _, indices = index.search(query_, k)
+    return indices
