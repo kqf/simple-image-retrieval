3d6cf4d7b2b8fca42d3c3c1a0d74ed698a3a8c31
Sasha <q.dev.null@gmail.com>
2021-09-22 09:55:13 +0200

    Add the sigmoid space

--- tests/conftest.py
+++ tests/conftest.py
@@ -36,14 +36,25 @@ def pytest_addoption(parser):
     )
 
 
+def sigmoid_space(start, stop, n_points, sigmoid_start=-10, sigmoid_stop=10):
+    # Define the
+    x = np.linspace(sigmoid_start,  sigmoid_stop, n_points)
+
+    # The sigmoid range is (0, 1)
+    sigmoid = 1. / (1. + np.exp(-x))
+
+    # Convert it to (stop, start)
+    return sigmoid * (stop - start) + start
+
+
 @pytest.fixture
 def fake_dataset(size=256, nfiles=10, fname="data.tsv", max_alpha=0.4):
     with tempfile.TemporaryDirectory() as dirname:
         path = Path(dirname)
         dataset = path / fname
-        lengths = np.linspace(0, max_alpha, nfiles)
-        lengths[:len(lengths) // 2] = lengths[:len(lengths) // 2] / 2
-        lengths[len(lengths) // 2:] = lengths[len(lengths) // 2:] + 0.2
+
+        lengths = sigmoid_space(0, max_alpha, nfiles)
+        print(lengths)
 
         with dump_list(dataset) as files:
             for i, alpha in enumerate(lengths):
